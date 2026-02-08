ca3876091eb531642b093db3932979b9c8aaf4ec
Vladimir Khramov <khramov.dev@gmail.com>
2026-02-08 16:29:55 +0700

    Lesson 3 homework

---
+++ lesson03/homework/OnboardingScreen.kt
@@ -0,0 +1,50 @@
+val skipButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_skip_button",
+    "onboarding_skip"
+)
+
+val continueButton = listOf(
+    MaterialButton::class.java,
+    "fragment_onboarding_forward_button",
+    "onboarding_continue"
+)
+
+val logoImage = listOf(
+    AppCompatImageView::class.java,
+    "imageViewCentered"
+)
+
+val addLanguageButton = listOf(
+    MaterialButton::class.java,
+    "addLanguageButton",
+    "onboarding_multilingual_add_language_text"
+)
+
+val languagesList = listOf(
+    RecyclerView::class.java,
+    "languagesList"
+)
+
+val languageLabel = listOf(
+    AppTextView::class.java,
+    "option_label"
+)
+
+val header = listOf(
+    AppTextView::class.java,
+    "primaryTextView"
+)
+
+val subHeader = listOf(
+    AppTextView::class.java,
+    "secondaryTextView"
+)
+
+val pageIndicator = listOf(
+    TabLayout::class.java,
+    "view_onboarding_page_indicator"
+)
+
+
+
